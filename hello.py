5e22c610a5cd28a2a93c1e6bcca66105b89170e8
Ricardo-Filgueiras <ricardofilgueiras70@gmail.com>
2023-10-01 19:15:11 +0000

    Add versão da tradução do zen Python

--- hello.py
+++ hello.py
@@ -30,3 +30,27 @@ Type "help", "copyright", "credits" or "license" for more information.
 - Namespaces are one honking great idea -- let's do more of those! '''
 
 #Poderia trazer a tradução tambem ...
+
+'''Pitão 3.10.0
+Digite “ajuda”, “copyright”, “créditos” ou “licença” para obter mais informações.
+>>> importe isso
+- O Zen do Python, de Tim Peters
+- Bonito é melhor que feio.
+- Explícito é melhor que implícito.
+- Simples é melhor que complexo.
+- Complexo é melhor que complicado.
+- Plano é melhor que aninhado.
+- Esparso é melhor que denso.
+- A legibilidade conta.
+- Casos especiais não são especiais o suficiente para quebrar as regras.
+- Embora a praticidade supere a pureza.
+- Os erros nunca devem passar silenciosamente.
+- A menos que seja explicitamente silenciado.
+- Diante da ambiguidade, recuse a tentação de adivinhar.
+- Deveria haver uma – e de preferência apenas uma – maneira óbvia de fazer isso.
+- Embora esse caminho possa não ser óbvio à primeira vista, a menos que você seja holandês.
+- Agora é melhor do que nunca.
+- Embora nunca seja melhor do que *agora*.
+- Se a implementação for difícil de explicar, é uma má ideia.
+- Se a implementação for fácil de explicar, pode ser uma boa ideia.
+- Namespaces são uma ótima ideia - vamos fazer mais deles!'''
